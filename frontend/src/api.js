ef533593230ce1463e9d8a3cb8774d0c2db500e9
bhavendra <bhavendrakumar007@gmail.com>
2025-10-03 22:03:16 +0530

    frontend connected

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // backend URL
+  baseURL: 'https://b-shop-1.onrender.com/api',
 });
 
 // Attach token if available
